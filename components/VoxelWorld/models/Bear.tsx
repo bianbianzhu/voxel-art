4ebff124c1a71c2036a881460b59fd1445cefb35
tianyili-roller <tianyi.li@rollerdigital.com>
2025-11-20 13:56:46 +1100

    feat: Enhance bear model with more detailed voxel parts and adjust the fox's initial position.

--- components/VoxelWorld/models/Bear.tsx
+++ components/VoxelWorld/models/Bear.tsx
@@ -3,12 +3,55 @@ import React from 'react';
 export const Bear: React.FC = () => {
     return (
         <>
-            <mesh position={[0, 0.6, 0]}>
-                <boxGeometry args={[0.8, 0.7, 1.2]} />
+            {/* Body */}
+            <mesh position={[0, 0.7, 0]}>
+                <boxGeometry args={[0.9, 0.8, 1.3]} />
                 <meshStandardMaterial color="#4A3728" />
             </mesh>
-            <mesh position={[0, 1.1, 0.5]}>
-                <boxGeometry args={[0.5, 0.5, 0.4]} />
+
+            {/* Head */}
+            <mesh position={[0, 1.2, 0.7]}>
+                <boxGeometry args={[0.6, 0.5, 0.5]} />
+                <meshStandardMaterial color="#4A3728" />
+            </mesh>
+
+            {/* Snout */}
+            <mesh position={[0, 1.1, 1.0]}>
+                <boxGeometry args={[0.25, 0.2, 0.2]} />
+                <meshStandardMaterial color="#6F5E53" /> {/* Lighter brown */}
+            </mesh>
+
+            {/* Ears */}
+            <mesh position={[0.2, 1.5, 0.6]}>
+                <boxGeometry args={[0.15, 0.15, 0.1]} />
+                <meshStandardMaterial color="#4A3728" />
+            </mesh>
+            <mesh position={[-0.2, 1.5, 0.6]}>
+                <boxGeometry args={[0.15, 0.15, 0.1]} />
+                <meshStandardMaterial color="#4A3728" />
+            </mesh>
+
+            {/* Legs */}
+            <mesh position={[0.3, 0.3, 0.4]}>
+                <boxGeometry args={[0.25, 0.6, 0.25]} />
+                <meshStandardMaterial color="#4A3728" />
+            </mesh>
+            <mesh position={[-0.3, 0.3, 0.4]}>
+                <boxGeometry args={[0.25, 0.6, 0.25]} />
+                <meshStandardMaterial color="#4A3728" />
+            </mesh>
+            <mesh position={[0.3, 0.3, -0.4]}>
+                <boxGeometry args={[0.25, 0.6, 0.25]} />
+                <meshStandardMaterial color="#4A3728" />
+            </mesh>
+            <mesh position={[-0.3, 0.3, -0.4]}>
+                <boxGeometry args={[0.25, 0.6, 0.25]} />
+                <meshStandardMaterial color="#4A3728" />
+            </mesh>
+
+            {/* Tail */}
+            <mesh position={[0, 0.8, -0.7]}>
+                <boxGeometry args={[0.15, 0.15, 0.15]} />
                 <meshStandardMaterial color="#4A3728" />
             </mesh>
         </>
